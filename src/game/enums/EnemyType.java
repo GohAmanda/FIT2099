e51066f6d2a5e47cbc76c67a2b023488b1e826c6
GohAmanda <104143769+GohAmanda@users.noreply.github.com>
2024-06-15 19:54:33 +0800

    Add files via upload

---
+++ src/game/enums/EnemyType.java
@@ -0,0 +1,13 @@
+package game.enums;
+
+/**
+ * The enum enemy type
+ *
+ * @author Amanda Goh, Samantha Oh, Tan Yi Jin
+ */
+public enum EnemyType {
+    SKELETAL,
+    DOGANDWOLF,
+    CRABANDFISH,
+    BROTHERS
+}
---
+++ src/game/enums/WeaponType.java
@@ -0,0 +1,13 @@
+package game.enums;
+
+/**
+ * The enum weapon type
+ *
+ * @author Amanda Goh, Samantha Oh, Tan Yi Jin
+ */
+public enum WeaponType {
+    DAGGER,
+    HAMMER,
+    CURVED_SWORD,
+    KATANA
+}
